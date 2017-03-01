8096ae512d89ee4b175b108a9717b6bf9e0f4f6a
jomilanez <josiane.milanez@gmail.com>
2017-03-01 22:06:30 +0100

    first commit

---
+++ src/main/java/com/sales/AquisitorApplication.java
@@ -0,0 +1,12 @@
+package com.sales;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AquisitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AquisitorApplication.class, args);
+	}
+}
